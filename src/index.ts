0e43e4728d4bc64b5b704c2c0c37cbd14f961a95
StireArLankar <stire.ru@gmail.com>
2019-08-01 15:20:40 +0300

    added static files + asset build script

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ import express from 'express'
 const app = express()
 const port = 8080 // default port to listen
 
+app.use(express.static(__dirname + '/public'))
+
 // define a route handler for the default home page
 app.get('/', (req, res) => {
-  res.send('Hello world!')
+  res.sendFile(__dirname + '/public/index.html')
 })
 
 // start the Express server
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
-  console.log(`server started at http://localhost:${ port }`)
+  console.log(`Server started at http://localhost:${ port }`)
 })
